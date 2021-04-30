14a212962ccad0b83c5d30fdf1f427d7fa19c24f
vlad <daprostovseeto@gmail.com>
2021-04-30 17:49:25 +0300

    add json file opt

--- k2.go
+++ k2.go
@@ -7,15 +7,15 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/puerkitobio/goquery"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strings"
-
-	"github.com/puerkitobio/goquery"
 )
 
 type sliceVal []string
@@ -42,6 +42,20 @@ func writeLines(lines []string, path string) error {
 	return w.Flush()
 }
 
+func writeJson(lines []string, path string) {
+	file, err := json.MarshalIndent(lines, "", "  ")
+
+	if err != nil {
+		log.Fatalf("[!] Couldn't create file: %s\n", err.Error())
+	}
+
+	err = ioutil.WriteFile(path, file, 0644)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func httpRequest(URI string) string {
 	response, errGet := http.Get(URI)
 	if errGet != nil {
@@ -57,55 +71,56 @@ func httpRequest(URI string) string {
 	return string(responseText)
 }
 
-func parseTable(data string) ([][]string, []string) {
-	var rows [][]string
-	var row []string
+func parseTable(data string) []string {
+	var domains []string
 
 	doc, err := goquery.NewDocumentFromReader(strings.NewReader(data))
 	if err != nil {
 		log.Fatalf("[!] Parsing issue: %s\n", err.Error())
 	}
 
-	doc.Find("table").Each(func(index int, tablehtml *goquery.Selection) {
-		tablehtml.Find("tr").Each(func(indextr int, rowhtml *goquery.Selection) {
-			rowhtml.Find("td").Each(func(indexth int, tablecell *goquery.Selection) {
-				row = append(row, tablecell.Text())
-			})
-			rows = append(rows, row)
-			row = nil
+	doc.Find("tr").Each(func(_ int, tr *goquery.Selection) {
+		tr.Find("td").Each(func(i int, row *goquery.Selection) {
+			if i == 1 {
+				domain := row.Text()
+				if domain != "" {
+					domains = append(domains, domain)
+				}
+			}
 		})
 	})
 
-	return rows, row
+	return domains[1:]
 }
 
 func main() {
 	namePtr := flag.String("n", "", "Registrant name, email or domain name of the target (Required)")
 	printPtr := flag.Bool("p", false, "Print results")
+	jsonF := flag.Bool("j", false, "Write to a json file")
 	flag.Parse()
 
 	if *namePtr == "" {
 		flag.PrintDefaults()
-		os.Exit(0)
+		log.Fatal("Registrant name, email or domain name of the target is required")
 	}
 
 	fmt.Println("[:] Sending query...")
 	request := httpRequest(fmt.Sprintf("https://www.reversewhois.io/?searchterm=%s", *namePtr))
-	rows, _ := parseTable(request)
+	domains := parseTable(request)
 
-	var domains []string
-
-	if len(rows) > 0 {
-		for _, domain := range rows[1:] {
-			domains = append(domains, domain[1])
-		}
+	if len(domains) > 0 {
 
 		if *printPtr {
 			fmt.Print(sliceVal(domains))
 		}
 
 		fmt.Printf("[:] Writing %d domain(s) to file...\n", len(domains))
-		writeLines(domains, "domains.txt")
+
+		if *jsonF {
+			writeJson(domains, "domains.json")
+		} else {
+			writeLines(domains, "domains.txt")
+		}
 	} else {
 		fmt.Println("[!] No domains found")
 	}
